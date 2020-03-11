fd283266bd2d82414dec52143ad11c3d4d916dc1
Tsarcasm <lockiecharles@gmail.com>
2020-03-11 19:20:23 +0000

    Added SyncStore

---
+++ src/main/java/uk/tsarcasm/tsorm/SyncStore.java
@@ -0,0 +1,18 @@
+package uk.tsarcasm.tsorm;
+
+public class SyncStore<T extends Entity> extends DatabaseStore<T> {
+  @Override
+  protected boolean db_save(T obj) {
+    return dbi.save(obj);
+  }
+
+  @Override
+  protected boolean db_delete(T obj) {
+    return dbi.delete(obj);
+  }
+
+  @Override
+  protected T db_create(T obj) {
+    return dbi.create(obj);
+  }
+}
