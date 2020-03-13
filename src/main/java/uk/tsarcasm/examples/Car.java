d132ccd5be44773b187e978846f466b1ccbaa4d5
Tsarcasm <lockiecharles@gmail.com>
2020-03-13 11:13:24 +0000

    Added some examples to demonstrate a tsorm setup

---
+++ src/main/java/uk/tsarcasm/examples/Car.java
@@ -0,0 +1,20 @@
+package uk.tsarcasm.examples;
+
+import uk.tsarcasm.tsorm.Entity;
+
+public class Car extends Entity {
+    public final int year;
+    public final String model;
+
+    // Reference to the company that made the car
+    public final int companyPk;
+
+
+
+    public Car(int pk, int year, String model, int companyPk) {
+        super(pk);
+        this.year = year;
+        this.model = model;
+        this.companyPk = companyPk;
+    }
+}
---
+++ src/main/java/uk/tsarcasm/examples/Company.java
@@ -0,0 +1,35 @@
+package uk.tsarcasm.examples;
+
+import uk.tsarcasm.tsorm.Entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Company extends Entity {
+    public final String name;
+    public final int numEmployees;
+    public final double netWorth;
+
+    public final Collection<Car> cars;
+
+
+    public Company(int pk, String name, int numEmployees, double netWorth, Collection<Car> cars) {
+        super(pk);
+        this.name = name;
+        this.numEmployees = numEmployees;
+        this.netWorth = netWorth;
+        this.cars = cars;
+    }
+
+    public Collection<Car> getCarsSinceYear(int year) {
+        List<Car> carsSince = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.year >= year) {
+                carsSince.add(car);
+            }
+        }
+        return carsSince;
+    }
+
+}
